9919340be5a415b06407976fc282965cc041ffe9
xfang <2553041586@qq.com>
2017-06-13 15:26:05 +0800

    第一次正式提交内容

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+
+  // TinyNav.js 
+  	$('#nav').tinyNav({ header: 'Navigation' });
+  
+  
+  // FitVid.js
+     $(".main-container").fitVids();
+     
+  // Fix Widows
+  	 $('p').widowFix();   
+ 
+ 
+  // Fancybox.js
+	 $('.fancybox').fancybox();
+	  
+  
+  
+  // BackStretch.js	 
+	 $("#masthead").backstretch("img/masthead@2x.jpg");
+	 
+	 
+});
+
+
